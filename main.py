c35d03a7476d2ec570cec716701ed77048b87dbe
sashank_reddy <sashankreddy238@gmail.com>
2026-01-07 13:47:50 +0530

    view option added

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+import json
 app=FastAPI()
-
+def load_data():
+    with open('patients.json','r') as f:
+        patientdata=json.load(f)
+        return patientdata
+        
 @app.get('/')
 def get():
     return {"message":"hello world"}
 @app.get('/sum')
 def sum():
     return {"message":"sum is displayed"}
+@app.get('/view')
+def view():
+    getview=load_data()
+    return getview
